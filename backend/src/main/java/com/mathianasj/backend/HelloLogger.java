88cbe1a3c54615052cfbb1c7ba187b1ccc0c0148
null <null>
2023-11-04 02:47:27 +0000

    add failing when directory does not exist

--- backend/src/main/java/com/mathianasj/backend/HelloLogger.java
+++ backend/src/main/java/com/mathianasj/backend/HelloLogger.java
@@ -5,15 +5,22 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 @Path("/hello")
 public class HelloLogger {
     @GET
     public String echo() throws IOException {
-        File f = File.createTempFile("/create/mount/here", "txt");
+        File dir = new File("/create/mount/here");
+        File f = File.createTempFile("present", ".txt", dir);
+        FileWriter fileWriter = new FileWriter(f.getName(), true);
+        BufferedWriter bw = new BufferedWriter(fileWriter);
+        bw.write("testing");
+        bw.close();
 
-        return "Hello";
+            return "Hello";
     }
 }
