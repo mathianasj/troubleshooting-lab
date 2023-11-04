cde40ffc4d10bb300cd2251f1aaf6a035f8a1da0
null <null>
2023-11-04 02:26:13 +0000

    fix build again

--- backend/src/main/java/com/mathianasj/backend/HelloLogger.java
+++ backend/src/main/java/com/mathianasj/backend/HelloLogger.java
@@ -6,11 +6,12 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 import java.io.File;
+import java.io.IOException;
 
 @Path("/hello")
 public class HelloLogger {
     @GET
-    public String echo() {
+    public String echo() throws IOException {
         File f = File.createTempFile("/create/mount/here", "txt");
 
         return "Hello";
